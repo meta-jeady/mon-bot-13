5c5835388503086d5b74a34b19d06b48fad864cb
META JEADY <djiogofoyet@gmail.com>
2026-08-15 13:46:23 +0100

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    OWNER_NUMBER: '237687960259',
+    BOT_NAME: 'META JEADY',
+    VERSION: 'v2.8.0',
+    PREFIX: '.',
+    SIGNATURE: '> BY : _© 2026 KČØ4P TECH_',
+    OWNER_NAME: 'KČØ4P',
+    MODE: 'public',
+    LOGO_PATH: './logo.jpg',
+    PING_BANNER: 'https://i.ibb.co/0yXk3vL/ping-banner.jpg',
+    SESSION_DIR: './session',
+    WELCOME_DEFAULT: true,
+    ANTILINK_DEFAULT: false
+}
